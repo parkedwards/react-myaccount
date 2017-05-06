8a88f4cb719f363d5cb02f1b416ab3d6ef372caa
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-05 17:18:32 -0700

    extracted route config object

---
+++ src/utils/routes.js
@@ -0,0 +1,16 @@
+//  ROUTING CONFIG
+// Figure out the best way to implement...
+// may need to place this config object within the rendering components
+
+const routes = [
+  {
+    path: '/account',
+    display: 'My Account',
+  },
+  {
+    path: '/settings',
+    display: 'Settings',
+  },
+];
+
+export default routes;
