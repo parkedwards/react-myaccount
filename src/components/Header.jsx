a3ee638cf8657195e19643b405215ab7e8790f3d
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-05 17:20:26 -0700

    placeholder header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h2>
+        App Header
+        <a href="">Log Out</a>
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
