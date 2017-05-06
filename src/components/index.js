23ef29d8369bf1fc351a5e97748b6faeaef4c1ff
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-05 17:20:19 -0700

    components index to reference

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// Reference-able index for Display Components
+
+export { default as Root } from './Root';
+export { default as App } from './App';
+
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as DisplaySection } from './DisplaySection';
+
+export { default as AccountDetail } from './AccountDetail';
+export { default as Settings } from './Settings';
