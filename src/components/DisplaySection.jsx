977a3987a28095f489152c02be9f8ae082c93c11
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-09 10:46:28 -0700

    passing user's first name into <DisplaySection> component

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom';
 
 import { AccountDetail, Settings } from '../components';
 
-const DisplaySection = () => {
+const DisplaySection = ({ firstName }) => {
   return (
     <div>
-      <Route exact path="/" render={() => <h2>Welcome!</h2>} />
+      <Route exact path="/" render={() => <h2>Welcome {firstName}!</h2>} />
       <Route path="/account" component={AccountDetail} />
       <Route path="/settings" component={Settings} />
     </div>
