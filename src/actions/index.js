88ae1d680495601b00a7a7a19d370004dcd3b41d
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-09 10:45:49 -0700

    defining action creators for async auth request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
+import fetch from 'isomorphic-fetch';
 import * as types from './ActionTypes';
 // maybe import isomorphic fetch
+
+// const API = 'https://infinite-crag-99070.herokuapp.com/authenticate';
+const API = 'http://localhost:3000/authenticate';
 
 export const selectView = view => (
   {
@@ -8,3 +12,36 @@ export const selectView = view => (
   }
 );
 
+export const requestUser = (username, password) => (
+  {
+    type: types.REQUEST_USER,
+    username,
+    password,
+  }
+);
+
+export const receiveUser = (payload) => (
+  {
+    type: types.RECEIVE_USER,
+    ...payload,
+  }
+);
+
+// Thunk
+export const fetchUser = (username, password) => (
+  dispatch => {
+    dispatch(requestUser(username, password)); // inform app state that API call is starting
+    return fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then(response => response.json())
+      .then(json => dispatch(receiveUser(json)));
+  }
+);
