b3fb03db708e20ae388c3fb4e548f5af6d93806e
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-09 10:46:53 -0700

    adding user fields to state tree

--- src/reducers/stateShape.js
+++ src/reducers/stateShape.js
@@ -1,6 +1,9 @@
 const INITIAL_STATE = {
   user: {
     id: null,
+    username: null,
+    name: null,
+    isFetching: false,
   },
   selectedSidebar: null,
 };
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,27 @@
 import * as types from '../actions/ActionTypes';
 
-const user = (state = null, action) => {
+const INITIAL_USER_STATE = {
+  _id: null,
+  username: null,
+  first_name: null,
+  last_name: null,
+  isFetching: false,
+};
+
+const user = (state = INITIAL_USER_STATE, action) => {
   switch (action.type) {
-    case 'LOG_IN':
-      return action.id;
+    case types.REQUEST_USER:
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case types.RECEIVE_USER:
+      return Object.assign({}, state, {
+        id: action._id,
+        username: action.username,
+        first_name: action.first_name,
+        last_name: action.last_name,
+        isFetching: false,
+      });
     default:
       return state;
   }
